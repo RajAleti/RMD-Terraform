620e02f59ca877f17e058314cba131ceb2c5db55
RajAleti <97768579+RajAleti@users.noreply.github.com>
2022-08-19 09:20:57 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "location" {
+  default = ""
+  type = string
+}
+
+variable "rg_name" {
+  default = ""
+  type = string
+}
+
+variable "solution" {
+  default = ""
+  type = string
+}
+
+variable "environment" {
+  default = ""
+  type = string
+}
+
+variable "db_fqdn" {
+  default = ""
+  type = string
+}
+
+variable "app_service_subnet_id" {
+  default = ""
+  type = string
+}
+
+variable "storage_account_name" {
+  default = ""
+  type = string
+}
+
+variable "storage_account_access_key" {
+  default = ""
+  type = string
+}
+
+variable "container_name" {
+  default = ""
+  type = string
+}
